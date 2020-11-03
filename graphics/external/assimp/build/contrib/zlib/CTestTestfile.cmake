c10a2fc07e77ba3a9bc71c8e519a8fb0bf6faf25
evo <blithe_han@163.com>
2020-11-03 11:03:25 +0800

    remove: remove build files for assimp

--- graphics/external/assimp/build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/evo/github/tr-cg/graphics/external/assimp/contrib/zlib
-# Build directory: F:/evo/github/tr-cg/graphics/external/assimp/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
